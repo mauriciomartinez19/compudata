eee29a60931175340031677879dd5fbe1d6a863c
Mauricio Martinez <maurimar19@hotmail.com>
2024-05-11 19:44:06 -0300

    Add mailto and call to contact section

--- src/app/ui/sections/contact/index.tsx
+++ src/app/ui/sections/contact/index.tsx
@@ -1,7 +1,8 @@
 import dynamic from "next/dynamic";
-import EmailForm from "../../components/email/EmailForm";
 import FancyBackground from "../FancyBackground";
 import Email from "../../components/email";
+import Image from "next/image";
+import Link from "next/link";
 
 const Map = dynamic(() => import("../../components/Map"), {
   loading: () => <div>Loading...</div>,
@@ -33,22 +34,44 @@ const Contact = ({ id }: { id: string }) => {
                             fill="none"
                             viewBox="0 0 24 24"
                             stroke="currentColor"
-                            className="mx-auto mb-6 h-8 w-8 text-primary dark:text-primary-400"
+                            className="mx-auto mb-6 h-8 w-8 text-primary"
                           >
                             <path d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
                           </svg>
                         </div>
                       </div>
                       <div className="ml-6 grow">
-                        <p className="mb-2 font-bold dark:text-white">
-                          Teléfono de contacto
-                        </p>
-                        <p className="text-neutral-500 dark:text-neutral-200">
-                          099 634 437
-                        </p>
-                        <p className="text-neutral-500 dark:text-neutral-200">
-                          2201 6524
-                        </p>
+                        <p className="mb-2 font-bold">Teléfono de contacto</p>
+                        <div className="flex flex-col">
+                          <div className="flex gap-3">
+                            <a
+                              className="text-white no-underline"
+                              target="_blank"
+                              href="tel:+59899624437"
+                            >
+                              099 634 437
+                            </a>
+                            <Link
+                              target="_blank"
+                              href="https://wa.me/+59899624437"
+                              className="flex align-items-center"
+                            >
+                              <Image
+                                alt="wpp-logo"
+                                src="/images/whatsapp-logo.svg"
+                                height={17}
+                                width={17}
+                              />
+                            </Link>
+                          </div>
+                          <a
+                            className="text-white no-underline"
+                            target="_blank"
+                            href="tel:2201 6524"
+                          >
+                            2201 6524
+                          </a>
+                        </div>
                       </div>
                     </div>
                   </div>
@@ -61,7 +84,7 @@ const Contact = ({ id }: { id: string }) => {
                             fill="none"
                             viewBox="0 0 24 24"
                             stroke="currentColor"
-                            className="mx-auto mb-6 h-8 w-8 text-primary dark:text-primary-400"
+                            className="mx-auto mb-6 h-8 w-8 text-primary"
                           >
                             <path d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
                             <path d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
@@ -69,12 +92,8 @@ const Contact = ({ id }: { id: string }) => {
                         </div>
                       </div>
                       <div className="ml-6 grow">
-                        <p className="mb-2 font-bold dark:text-white">
-                          Localización
-                        </p>
-                        <p className="text-neutral-500 dark:text-neutral-200">
-                          Gral Enrique Martínez 1311
-                        </p>
+                        <p className="mb-2 font-bold">Localización</p>
+                        <p className="text-white">Gral Enrique Martínez 1311</p>
                       </div>
                     </div>
                   </div>
@@ -87,20 +106,30 @@ const Contact = ({ id }: { id: string }) => {
                             xmlns="http://www.w3.org/2000/svg"
                             viewBox="0 0 32 32"
                             fill="white"
-                            className="mx-auto mb-6 h-8 w-8 text-primary dark:text-primary-400"
+                            className="mx-auto mb-6 h-8 w-8 text-primary"
                           >
                             <path d="M29 4H3a3 3 0 0 0-3 3v18a3 3 0 0 0 3 3h26a3 3 0 0 0 3-3V7a3 3 0 0 0-3-3zm-.72 2L16 14.77 3.72 6zM30 25a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V7.23l13.42 9.58a1 1 0 0 0 1.16 0L30 7.23z" />
                           </svg>
                         </div>
                       </div>
                       <div className="ml-6 grow">
-                        <p className="mb-2 font-bold dark:text-white">Email</p>
-                        <p className="text-neutral-500 dark:text-neutral-200">
-                          compudata@compudata.com
-                        </p>
-                        <p className="text-neutral-500 dark:text-neutral-200">
-                          jamar2006@gmail.com
-                        </p>
+                        <p className="mb-2 font-bold">Email</p>
+                        <div className="flex flex-col">
+                          <a
+                            className="text-white no-underline"
+                            target="_blank"
+                            href="mailto:compudata@compudata.com.uy"
+                          >
+                            compudata@compudata.com.uy
+                          </a>
+                          <a
+                            className="text-white no-underline"
+                            target="_blank"
+                            href="mailto:jamar2006@gmail.com"
+                          >
+                            jamar2006@gmail.com
+                          </a>
+                        </div>
                       </div>
                     </div>
                   </div>
